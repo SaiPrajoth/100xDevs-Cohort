4940637b54593b5eb6541db1eae7500f493f8c11
Sai Prajoth <87938462+SaiPrajoth@users.noreply.github.com>
2024-01-29 05:09:35 +0530

    Update zod-input-validation.js

--- 0-100/week-5/5.2/zod-input-validation.js
+++ 0-100/week-5/5.2/zod-input-validation.js
@@ -1,11 +1,23 @@
+// Importing the Zod library
 const z = require('zod');
+
+// Defining a Zod schema for creating a todo item
 const CreateTodo = z.object({
-    title:z.string(),
-    description:z.string()
+    // 'title' field must be a string
+    title: z.string(),
+    // 'description' field must be a string
+    description: z.string()
 })
 
+// Defining a Zod schema for updating a todo item
 const UpdateTodo = z.object({
-    completed:z.string()
+    // 'completed' field must be a string
+    completed: z.string()
 })
 
-const response = CreateTodo.safeParse({title:"title",description:"description"}); // validates if the inputs types are as specified
+// Attempting to parse an object using the CreateTodo schema
+const response = CreateTodo.safeParse({ // validates if the inputs types are as specified
+    title: "title",
+    description: "description"
+});
+ 
